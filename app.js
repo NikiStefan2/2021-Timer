f2f0c9dfa70af5810a339f38fb356ec38caf9062
Niki <nikistefan95@gmail.com>
2020-12-26 15:24:04 +0200

    addedd alll nouirifaction sbye poa pa

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let CountDown2021 = new Date("Dec 31, 2020 00:00:00").getTime();
+let CountDown2021 = new Date("Jan 1, 2021 00:00:00").getTime();
 
-let n = setInterval(function(){
+let n = setInterval(function () {
 
 
     let now = new Date().getTime();
@@ -12,10 +12,13 @@ let n = setInterval(function(){
     let minute = Math.floor((distanta % (1000 * 60 * 60)) / (1000 * 60));
     let secunde = Math.floor((distanta % (1000 * 60)) / 1000);
 
-    document.getElementsByClassName("count-down-2021")[0].innerHTML = days + " zile " + ore + " ore " + minute + " minute " + secunde + " secunde ";
-
-    if(distanta < 0){
+    //document.getElementsByClassName("count-down-2021")[0].innerHTML = days + " zile " + ore + " ore " + minute + " minute " + secunde + " secunde ";
+    document.getElementsByClassName("count-down-2021-days")[0].innerHTML = days + " zile ";
+    document.getElementsByClassName("count-down-2021-hours")[0].innerHTML = ore + " ore ";
+    document.getElementsByClassName("count-down-2021-minutes")[0].innerHTML = minute + " minute ";
+    document.getElementsByClassName("count-down-2021-seconds")[0].innerHTML = secunde + " secunde ";
+    if (distanta < 0) {
         clearInterval(n);
         document.getElementsByClassName("count-down-2021")[0].innerHTML = "An nou fericit!!!";
     }
-})
+}, 1000);
